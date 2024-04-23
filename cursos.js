fa9ac8efa8e571cd94a4b1921464f7e7180d4db0
Filipe Silva <120770063+a2022113664@users.noreply.github.com>
2024-04-24 00:23:54 +0100

    Update cursos.js

--- cursos.js
+++ cursos.js
@@ -1,5 +1,5 @@
 async function createCurso() {
-  const apiUrl = "http://127.0.0.1:8000/api/cursos";
+  const apiUrl = "http://127.0.0.1:80/api/cursos";
 
   let nome = document.querySelector("#nome").value;
   let anos = Number(document.querySelector("#anos").value);
